b95e36957b3c4166495a39ee61f30e5e5a792a3a
joelbendict <joelbendict24@gmail.com>
2022-01-15 17:06:33 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'Login.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  /*  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.blue)); */
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          )),
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    );
+  }
+}
